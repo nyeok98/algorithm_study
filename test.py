099c95d8279fc4eb9c6c706477908aee3acd7f9d
Dongnyeok Shin <dn.shin98@gmail.com>
2021-02-18 23:34:26 +0900

    [learn] binary in some situation

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
-import sys
+n, m = list(map(int, input().split()))
+array = list(map(int, input().split()))
 
-input_data = sys.stdin.readline().rstrip()
+start = 0
+end = max(array)
 
-print(input_data)
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for x in array:
+        if x > mid:
+            total += x - mid
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
