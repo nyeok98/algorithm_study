923152a64b366b01d11beaf98705d525a2921697
Dongnyeok Shin <dn.shin98@gmail.com>
2021-02-19 23:27:53 +0900

    [solve] dp_p1

---
+++ dp/dp_p1.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+data = list(map(int, input().split()))
+
+d = [0]*101
+d[1] = data[0]
+d[2] = max(data[0], data[1])
+
+for i in range(3, n):
+    d[i] = max(d[i-1], d[i-2] + data[i-1])
+
+print(d[n-1])
