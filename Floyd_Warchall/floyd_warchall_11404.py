7e4be312bacc168d473d0299b01706951814e1a7
Dongnyeok Shin <dn.shin98@gmail.com>
2021-02-25 23:34:01 +0900

    [study] floyd-warchall

--- Floyd_Warchall/floyd_warchall_11404.py
+++ Floyd_Warchall/floyd_warchall_11404.py
@@ -1,17 +1,22 @@
+import sys
+input = sys.stdin.readline
 INF = int(1e9)
 
 n = int(input())
 m = int(input())
 
 graph = [[INF]*(n+1) for _ in range(n+1)]
-for a in range(n+1):
-    for b in range(n+1):
+for a in range(1, n+1):
+    for b in range(1, n+1):
         if a==b:
             graph[a][b] = 0
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    graph[a][b] = c
+    if graph[a][b] != INF:
+        graph[a][b] = min(graph[a][b], c)
+    else:
+        graph[a][b] = c
 
 for k in range(1, n+1):
     for a in range(1, n+1):
@@ -21,6 +26,8 @@ for k in range(1, n+1):
 for a in range(1, n+1):
     for b in range(1, n+1):
         if graph[a][b] == INF:
-            print("INF", end=" ")
+            print(0, end=" ")
         else:
             print(graph[a][b], end=" ")
+
+    print()
